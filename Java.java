4aa22311feea118343a44521e97d76cf97d12c44
Chris <chrisdias2311@gmail.com>
2023-10-31 07:31:54 +0530

    Final Commit

--- Java.java
+++ Java.java
@@ -1,8 +1,5 @@
-class Java
-{
-public static void main (String args[])
-{
-System.out.println("Your name is "+args[0]);
-System.out.println("Your surname is "+args[1]);
-}
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("Your name is: "+ args[0] + " " + args[1]);
+    }
 }
